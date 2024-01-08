9ceabbfb08b7ccd98b4b9bf268ced0a79c4046e7
RukmalFernando <66022861+RukmalFernando@users.noreply.github.com>
2024-01-08 22:41:02 +0530

    Create ApiApplication.java

---
+++ api/src/main/java/com/hospital_ms/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.hospital_ms.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
