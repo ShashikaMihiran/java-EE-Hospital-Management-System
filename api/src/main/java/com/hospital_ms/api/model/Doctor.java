511ae96d91b692a2f5906274744bd23444884ace
RukmalFernando <66022861+RukmalFernando@users.noreply.github.com>
2024-01-08 22:41:06 +0530

    Create Doctor.java

---
+++ api/src/main/java/com/hospital_ms/api/model/Doctor.java
@@ -0,0 +1,43 @@
+package com.hospital_ms.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // make the id to auto incremented...
+    private int doctorId;
+    private String doctorName;
+    private String specialization;
+    
+    public Doctor() {
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getDoctorName() {
+        return doctorName;
+    }
+
+    public void setDoctorName(String doctorName) {
+        this.doctorName = doctorName;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specilazation) {
+        this.specialization = specilazation;
+    }
+    
+}
