2c8a878645f63040bf01d525d8210e1645802d2d
RukmalFernando <66022861+RukmalFernando@users.noreply.github.com>
2024-01-08 22:41:26 +0530

    Create ApiApplicationTests.java

---
+++ api/src/test/java/com/hospital_ms/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital_ms.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
